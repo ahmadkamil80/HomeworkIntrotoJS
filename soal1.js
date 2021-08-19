c1e9f01650dc6b3059ac9baae96e2f286a0a5ac3
AhmadKamil <ahmadkamil80@gmail.com>
2021-08-19 19:38:10 +0700

    Merubah huruf string yang bernilai vokal menjadi bintang

--- soal1.js
+++ soal1.js
@@ -1,21 +1,15 @@
-let message = "The quick brown fox jumps over the lazy dog";
-
-let hasil = "";
-
-for (let i = 0; i < message.length; i++) {
-    
-    if (message[i] === 'a' ) {
-        hasil += '*'
-    } else if (message[i] === 'i'){
-        hasil += '*'
-    } else if (message[i] === 'u'){
-        hasil += '*'
-    } else if (message[i] === 'e'){
-        hasil += '*'
-    } else if (message[i] === 'o'){
-        hasil += '*'
-    } else {
-        hasil += message[i]
-    }
-    console.log(hasil)
-}
+const message = 'The quick brown fox jumps over the lazy dog';
+const replaceVowels = (message = '') => {
+   const vocal = 'aeiou';
+   let res = '';
+   for(let i = 0; i < message.length; i++){
+      const el = message[i];
+      if(vocal.includes(el)){  
+         res += ('*');
+      }else{
+         res += el;
+      };
+   };
+   return res;
+};
+console.log(replaceVowels(message));
